36849da35906972ffdace16016090b46c0f37d2a
mercy vincent <mercydanke@gmail.com>
2024-12-28 19:43:46 +0100

    lib uv thread pool

--- node/nodejs-advanced-concepts/thread.js
+++ node/nodejs-advanced-concepts/thread.js
@@ -1,6 +1,24 @@
+process.env.UV_THREADPOOL_SIZE = 6 
+
 const crypto = require('crypto');
 
 const start = Date.now();
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    console.log('amount of time in sec it takes to cal the hash is:', Date.now() - start);
+    console.log('1) amount of time in sec it takes to cal the hash is:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2) amount of time in sec it takes to cal the hash is:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3) amount of time in sec it takes to cal the hash is:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4) amount of time in sec it takes to cal the hash is:', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5) amount of time in sec it takes to cal the hash is:', Date.now() - start);
 });
